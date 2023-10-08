39d58681869de0768a6eaa986f550fd72be0c1d6
uk8096386837@gmail.com <uk8096386837@gmail.com>
2023-10-08 20:58:53 +0530

    feating the data fromt he server every 1 second

--- public/userinterface/script.js
+++ public/userinterface/script.js
@@ -2,7 +2,7 @@ const usermessage = document.getElementById("chatinputmessage");
 const chatContainer = document.getElementById("chat-messages");
 const sendbtn = document.getElementById("messagesend");
 const token = localStorage.getItem("token");
-var Name='';
+var Name = "";
 
 const sendmessage = async (messagedata) => {
   let div = document.createElement("div");
@@ -15,42 +15,48 @@ const sendmessage = async (messagedata) => {
   try {
     await axios.post("http://localhost:4000/message/messagedata", obj, {
       headers: { Authorization: token },
-    });    
+    });
   } catch (err) {
     console.log(err);
     // Handle the error, e.g., show an error message to the user
   }
 };
 
-const fetchMessages = async () => {
-  try {
-    const messages = await axios.get(
-      "http://localhost:4000/message/fetchmessage",
-      {
-        headers: { Authorization: token },
+const fetchMessages = () => {
+    setInterval(async () => {
+      try {
+        const messagesResponse = await axios.get("http://localhost:4000/message/fetchmessage", {
+          headers: { Authorization: token },
+        });
+        const messages = messagesResponse.data.messages;
+        const id = messagesResponse.data.userId;
+        let Name = messagesResponse.data.name;
+        chatContainer.innerHTML = '';
+  
+        messages.forEach((data) => {
+          let div = document.createElement("div");
+          let senderName = data.name;
+          let message = data.message;
+          let userId = data.userId;
+          if (userId === id) {
+            div.setAttribute("class", "sent-message");
+            div.innerHTML = `<p><p class="username">${senderName}:</p>${message}</p>`;
+            chatContainer.appendChild(div);
+          } else {
+            div.setAttribute("class", "received-message");
+            div.innerHTML = `<p><p class="username">${senderName}:</p>${message}</p>`;
+            chatContainer.appendChild(div);
+          }
+        });
+      } catch (err) {
+        console.log(err);
       }
-    );
-    let id = messages.data.userId;
-    Name = messages.data.name;
-    messages.data.messages.forEach((data) => {
-      let div = document.createElement("div");
-      let name = data.name;
-      let message = data.message;
-      let userId = data.userId;
-      if (userId === id) {
-        div.setAttribute("class", "sent-message"); // Corrected this line
-        div.innerHTML = `<p><p class="username">${name}:</p>${message}</p>`;
-        chatContainer.appendChild(div);
-      } else {
-        div.setAttribute("class", "received-message"); // Corrected this line
-        div.innerHTML = `<p><p class="username">${name}:</p>${message}</p>`;
-        chatContainer.appendChild(div);
-      }
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+    }, 5000); // Set the interval time in milliseconds (e.g., 5000 ms = 5 seconds)
+  };
+  
+  // Call the function to start fetching messages
+  fetchMessages();
+  
 
 sendbtn.addEventListener("click", () => {
   const message = usermessage.value;
@@ -59,22 +65,20 @@ sendbtn.addEventListener("click", () => {
   usermessage.value = "";
 });
 
-const currentmessage= async ()=> {
-    try {
-        const messages = await axios.get(
-          "http://localhost:4000/message/fetchmessage",
-          {
-            headers: { Authorization: token },
-          }
-        );
-        let id = messages.data.userId;
-        let name = messages.data.name;
-        
-        
-      } catch (err) {
-        console.log(err);
+const currentmessage = async () => {
+  try {
+    const messages = await axios.get(
+      "http://localhost:4000/message/fetchmessage",
+      {
+        headers: { Authorization: token },
       }
-}
+    );
+    let id = messages.data.userId;
+    let name = messages.data.name;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 fetchMessages();
-console.log(name)
+console.log(name);
